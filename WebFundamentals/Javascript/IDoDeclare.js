572aa950cebc5ac25d4eb8ef2add73efc6f23408
mlmcgowa <melindalmcgowan@gmail.com>
2017-07-17 15:01:18 -0500

    edited python project and added i do delace assignment

---
+++ WebFundamentals/Javascript/IDoDeclare.js
@@ -0,0 +1,19 @@
+var num = 24.2;
+var add = 30;
+var sub = 20.8;
+var mul = 1;
+
+var f_name = "Melinda";
+var l_name = "McGowan";
+var m_name = "Lester";
+
+var codingIsFun = true;
+var codingIsLame = false;
+
+var fullname = "";
+
+console.log(num + add + sub + mul);
+console.log(l_name, m_name, f_name);
+console.log(codingIsFun);
+console.log(codingIsLame);
+console.log(fullname);
