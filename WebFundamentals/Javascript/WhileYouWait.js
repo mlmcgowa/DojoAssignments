1aadad637cc40dd661b178b35f3cd61c9327228a
mlmcgowa <melindalmcgowan@gmail.com>
2017-07-17 23:22:02 -0500

    added While you waitassignment

---
+++ WebFundamentals/Javascript/WhileYouWait.js
@@ -0,0 +1,23 @@
+var days = 60;
+
+while (days > 30){
+  console.log(days + " days until my birthday. Such a long time... :(");
+  days= days -1;
+}
+
+while (days > 5){
+  console.log(days + " days until my birthday ..it is getting closer!!");
+  days= days -1;
+}
+
+while (days > 1){
+  console.log(days + " DAYS UNTIL MY BIRTHDAY!!!");
+  days= days -1;
+}
+
+while (days > 0){
+  console.log(days + " DAY UNTIL MY BIRTHDAY!!!");
+  days= days -1;
+}
+
+console.log("HAPPY BIRTHDAY PARTY TIME!!!");
