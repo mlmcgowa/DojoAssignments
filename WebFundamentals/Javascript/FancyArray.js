13c22b19de73e36162ceb7217da37ce994a9716e
mlmcgowa <melindalmcgowan@gmail.com>
2017-07-18 23:11:35 -0500

    updated fancy array assignment

--- WebFundamentals/Javascript/FancyArray.js
+++ WebFundamentals/Javascript/FancyArray.js
@@ -2,7 +2,7 @@ var arr = ["Melinda", "Matthew", "Katie", "Jordan"];
 
 function fancyNames() {
   for (var i = 0; i < arr.length; i++) {
-    console.log(i + arr[i]);
+    console.log(i + "->" + arr[i]);
   }
 }
 fancyNames();
