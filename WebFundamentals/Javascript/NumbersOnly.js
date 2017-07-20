0ddb9f8c4a9159a3d20b8b0057790a2bc7841e29
mlmcgowa <melindalmcgowan@gmail.com>
2017-07-19 19:41:34 -0500

    edited numbers only assignment

--- WebFundamentals/Javascript/NumbersOnly.js
+++ WebFundamentals/Javascript/NumbersOnly.js
@@ -2,7 +2,6 @@ var Arr = [1, "apple", -3, "orange", 0.5];
 var numbersOnly = []
 
 for (var i = 0; i < Arr.length; i++) {
-  console.log(Arr[i]);
   if (typeof Arr[i] === "number"){
     numbersOnly.push(Arr[i]);
   }
