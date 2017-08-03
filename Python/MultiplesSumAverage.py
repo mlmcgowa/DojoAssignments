0769b286220ad34f545022abcb1658072bd02328
mlmcgowa <melindalmcgowan@gmail.com>
2017-08-03 13:15:01 -0500

    added multiples sum average assignment

---
+++ Python/MultiplesSumAverage.py
@@ -0,0 +1,27 @@
+# Multiples
+# Part 1
+# print odd num 1 to 1000, with loop and no list
+
+# for i in range(1001):
+#     if i % 2 != 0:
+#         print i
+
+# Part 2
+# print all num 5 to 1,000,000, by multiples of 5
+
+# for i in range(1000001):
+#     if i % 5 == 0:
+#         print i
+
+# Sum List
+# print the sum of the values in the list a = [1, 2, 5, 10, 255, 3]
+
+# a = [1, 2, 5, 10, 255, 3]
+# print sum(a)
+
+# Average List
+# print the average of the values in the list a = [1, 2, 5, 10, 255, 3]
+
+a = [1, 2, 5, 10, 255, 3]
+average = sum(a) / len(a)
+print average 
