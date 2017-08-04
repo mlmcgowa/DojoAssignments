4c06dd454c349d449ce8c925b6dddf85089d21b9
mlmcgowa <melindalmcgowan@gmail.com>
2017-08-03 20:37:56 -0500

    added compare list assignment

---
+++ Python/CompareLists.py
@@ -0,0 +1,11 @@
+# Write a program that compares two lists and prints a message depending on if
+# the inputs are identical or not.
+
+list_one = [1,2,5,6,2]
+list_two = [1,2,5,6,2]
+
+if list_one == list_two:
+    print "These lists are the same!"
+
+else:
+    print "These lists are different!"
