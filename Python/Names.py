8a5f679246303b90b802f955e9b60f22be046d93
mlmcgowa <melindalmcgowan@gmail.com>
2017-08-08 11:44:18 -0500

    added names assignment

---
+++ Python/Names.py
@@ -0,0 +1,41 @@
+# Part 1
+
+# students = [
+#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#      {'first_name' : 'John', 'last_name' : 'Rosales'},
+#      {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#      {'first_name' : 'KB', 'last_name' : 'Tonel'}
+# ]
+
+# def names(arr):
+#     for x in arr:
+#         print x['first_name'], x['last_name']
+
+# names(students)
+
+# Part 2
+
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+
+def names(dict):
+    count = 0
+    for key in dict:
+        print key
+        for i in dict[key]:
+            count += 1
+            print count, "- " + i['last_name'], i['first_name'], len(i['last_name']) + len(i['first_name'])
+
+
+names(users)
